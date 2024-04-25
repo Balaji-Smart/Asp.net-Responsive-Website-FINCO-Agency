fc8834753e0e815d1e0ad8484497dcde1e505989
Balaji K <smartbalaji050@gmail.com>
2024-04-25 11:53:43 +0530

    Add files via upload

---
+++ Content/JavaScript.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    // Get the current year
+    const currentYear = new Date().getFullYear();
+
+    // Set the current year in the HTML
+    const copyrightYearSpan = document.getElementById('copyrightYear');
+    if (copyrightYearSpan) {
+        copyrightYearSpan.innerText = currentYear;
+    }
+});
